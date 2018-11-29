2adefa07df9f0fff4161d2459ac613c41cec1383
shinkarom <shinkarom@gmail.com>
2018-11-29 19:16:36 +0200

    Factored RGB into its own class

--- colormixer.js
+++ colormixer.js
@@ -1,5 +1,17 @@
 var symbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
 
+class RGB {
+  constructor (r, g, b) {
+    this.r = r
+    this.g = g
+    this.b = b
+  }
+
+  toHex () {
+    return '#' + numberToHex(this.r) + numberToHex(this.g) + numberToHex(this.b)
+  }
+}
+
 function changePos (x, y, pos) {
   var diff = y - x
   var percent = pos / 256
@@ -19,17 +31,17 @@ function hexToRGB (hex) {
   var r = hexToNumber(hex[1], hex[2])
   var g = hexToNumber(hex[3], hex[4])
   var b = hexToNumber(hex[5], hex[6])
-  return [r, g, b]
+  return new RGB(r, g, b)
 }
 
 function rgbToHex (r, g, b) {
   return '#' + numberToHex(r) + numberToHex(g) + numberToHex(b)
 }
 
-function mixColors (r1, g1, b1, r2, g2, b2, pos) {
-  var r3 = changePos(r1, r2, pos)
-  var g3 = changePos(g1, g2, pos)
-  var b3 = changePos(b1, b2, pos)
+function mixColors (color1, color2, pos) {
+  var r3 = changePos(color1.r, color2.r, pos)
+  var g3 = changePos(color1.g, color2.g, pos)
+  var b3 = changePos(color1.b, color2.b, pos)
   return rgbToHex(r3, g3, b3)
 }
 
@@ -38,8 +50,8 @@ function getValue (sel) {
 }
 
 function setRes (num, color) {
-  document.querySelector('#color' + num + 't').value = color
-  var res = document.querySelector('#color' + num)
+  document.querySelector(`#color${num}t`).value = color
+  var res = document.querySelector(`#color${num}`)
   res.innerText = color
   res.style.color = color
   res.style.backgroundColor = color
@@ -69,26 +81,25 @@ function bodyLoad () {
   mix()
 }
 
+function setRGBSwatches (color, number) {
+  document.querySelector(`#r${number}`).value = color.r
+  document.querySelector(`#r${number}t`).innerText = color.r
+  document.querySelector(`#g${number}`).value = color.g
+  document.querySelector(`#g${number}t`).innerText = color.g
+  document.querySelector(`#b${number}`).value = color.b
+  document.querySelector(`#b${number}t`).innerText = color.b
+}
+
 function editMix () {
   var pos = document.querySelector('#pos').value
   document.querySelector('#post').innerText = getPropFromPos(pos)
   var color1 = document.querySelector('#color1t').value
   var color2 = document.querySelector('#color2t').value
-  var [r1, g1, b1] = hexToRGB(color1)
-  var [r2, g2, b2] = hexToRGB(color2)
-  var color3 = (pos === 255) ? color2 : mixColors(r1, g1, b1, r2, g2, b2, pos)
-  document.querySelector('#r1').value = r1
-  document.querySelector('#r1t').innerText = r1
-  document.querySelector('#g1').value = g1
-  document.querySelector('#g1t').innerText = g1
-  document.querySelector('#b1').value = b1
-  document.querySelector('#b1t').innerText = b1
-  document.querySelector('#r2').value = r2
-  document.querySelector('#r2t').innerText = r2
-  document.querySelector('#g2').value = g2
-  document.querySelector('#g2t').innerText = g2
-  document.querySelector('#b2').value = b2
-  document.querySelector('#b2t').innerText = b2
+  var rgb1 = hexToRGB(color1)
+  var rgb2 = hexToRGB(color2)
+  var color3 = (pos === 255) ? color2 : mixColors(rgb1, rgb2, pos)
+  setRGBSwatches(rgb1, 1)
+  setRGBSwatches(rgb2, 2)
   setRes(1, color1)
   setRes(2, color2)
   setRes(3, color3)
@@ -109,9 +120,11 @@ function mix () {
   document.querySelector('#b2t').innerText = b2
   var pos = document.querySelector('#pos').value
   document.querySelector('#post').innerText = getPropFromPos(pos)
+  var rgb1 = new RGB(r1, g1, b1)
+  var rgb2 = new RGB(r2, g2, b2)
   var color1 = rgbToHex(r1, g1, b1)
   var color2 = rgbToHex(r2, g2, b2)
-  var color3 = (pos === 255) ? color2 : mixColors(r1, g1, b1, r2, g2, b2, pos)
+  var color3 = (pos === 255) ? color2 : mixColors(rgb1, rgb2, pos)
   setRes(1, color1)
   setRes(2, color2)
   setRes(3, color3)
